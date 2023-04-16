f9319d647ab78293c3e782e6dffd4652c7822ac6
João Gabriel Lima Marinho <jg.limamarinho202@gmail.com>
2023-04-16 17:21:36 -0300

    feat: facebook pixel + caroussel contents

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,12 @@ import { HomeCointainer } from "../styles/components/home";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import Link from "next/link";
+import * as fbq from "../lib/fpixel";
 
 const Home: NextPage = () => {
+  const handleClick = () => {
+    fbq.event("InitiateCheckout");
+  };
   return (
     <HomeCointainer>
       <Head>
@@ -56,6 +60,7 @@ const Home: NextPage = () => {
             <span className="self-end"> ou 27,00 a vista</span>
           </div>
           <Link
+            onClick={() => handleClick()}
             href={"https://pay.kiwify.com.br/QZaFymQ"}
             className="mt-10 rounded-full  bg-[#FFC700] px-7 py-4  "
           >
@@ -148,7 +153,7 @@ const Home: NextPage = () => {
           </div>
         </section>
 
-        <section className="py-12 flex flex-col items-center w-full bg-gradient-to-b to-[#5CE1E6] from-[#8C52FF] via-65% via-[#7A88F5]  p-5">
+        <section className="py-12 flex flex-col items-center w-full bg-gradient-to-b to-[#5CE1E6] from-[#8C52FF] via-65% via-[#7A88F5]">
           <div className="flex items-center flex-col w-full">
             <img src="/logo.png" alt="" className=""></img>
             <h2 className="uppercase font-bold text-2xl text-center ">
@@ -159,9 +164,9 @@ const Home: NextPage = () => {
                 aria-label="My Favorite Images"
                 options={{
                   rewind: true,
-                  padding: "50px",
+                  padding: "20px",
 
-                  gap: "20px",
+                  gap: "10px",
                   perPage: 3,
                   breakpoints: {
                     870: {
@@ -174,13 +179,109 @@ const Home: NextPage = () => {
                 }}
               >
                 <SplideSlide>
-                  <div className="w-full bg-white h-[350px] rounded-3xl "></div>
+                  <div className="w-full bg-[#f7f7f7] min-h-[450px] px-2 py-8 rounded-3xl flex flex-col justify-center items-center">
+                    <img
+                      src="/conteudos/1.jpg"
+                      alt=""
+                      className="w-full max-w-[250px]"
+                    />
+                    <h3 className="text-center text-[#472192] text-2xl font-bold mt-3">
+                      SUPER APOSTILA EDUCA KIDS
+                    </h3>
+                    <p className="text-center text-[#472192] mt-3 px-5">
+                      Atividades infantis para o desenvolvimento acelerado, com
+                      muitos exercícios e maneiras diferentes de ensinar e
+                      aprender, juntamente com alguns bônus.
+                    </p>
+                  </div>
                 </SplideSlide>
                 <SplideSlide>
-                  <div className="w-full bg-white h-[350px] rounded-3xl "></div>
+                  <div className="w-full bg-[#f7f7f7] min-h-[450px] px-2 py-8 rounded-3xl flex flex-col justify-center items-center">
+                    <img
+                      src="/conteudos/2.jpg"
+                      alt=""
+                      className="w-full max-w-[250px]"
+                    />
+                    <h3 className="text-center text-[#472192] text-2xl font-bold mt-3">
+                      15 ATIVIDADES COORDENAÇÃO MOTORA
+                    </h3>
+                    <p className="text-center text-[#472192] mt-3 px-5">
+                      A coordenação motora é uma habilidade fundamental para o
+                      desenvolvimento, é importante trabalhar a coordenação
+                      motora desde cedo, pois isso pode ajudar a melhorar a
+                      concentração.
+                    </p>
+                  </div>
                 </SplideSlide>
                 <SplideSlide>
-                  <div className="w-full bg-white h-[350px] rounded-3xl "></div>
+                  <div className="w-full bg-[#f7f7f7] min-h-[450px] px-2 py-8 rounded-3xl flex flex-col justify-center items-center">
+                    <img
+                      src="/conteudos/3.jpg"
+                      alt=""
+                      className="w-full max-w-[250px]"
+                    />
+                    <h3 className="text-center text-[#472192] text-2xl font-bold mt-3">
+                      +42 ATIVIDADES DE PORTUGUÊS
+                    </h3>
+                    <p className="text-center text-[#472192] mt-3 px-5">
+                      As atividades de português são extremamente importantes
+                      para o desenvolvimento das crianças. Elas ajudam a
+                      melhorar a compreensão e a expressão da língua, além de
+                      estimular a imaginação e a criatividade.
+                    </p>
+                  </div>
+                </SplideSlide>
+                <SplideSlide>
+                  <div className="w-full bg-[#f7f7f7] min-h-[450px] px-2 py-8 rounded-3xl flex flex-col justify-center items-center">
+                    <img
+                      src="/conteudos/4.jpg"
+                      alt=""
+                      className="w-full max-w-[250px]"
+                    />
+                    <h3 className="text-center text-[#472192] text-2xl font-bold mt-3">
+                      +20 ATIVIDADES DE MATEMÁTICA
+                    </h3>
+                    <p className="text-center text-[#472192] mt-3 px-5">
+                      As atividades de matemática são cruciais para o
+                      desenvolvimento das crianças. Elas ajudam a desenvolver
+                      habilidades importantes, como o raciocínio lógico, a
+                      resolução de problemas e a compreensão de conceitos
+                      matemáticos básicos.
+                    </p>
+                  </div>
+                </SplideSlide>
+                <SplideSlide>
+                  <div className="w-full bg-[#f7f7f7] min-h-[450px] px-2 py-8 rounded-3xl flex flex-col justify-center items-center">
+                    <img
+                      src="/conteudos/5.jpg"
+                      alt=""
+                      className="w-full max-w-[250px]"
+                    />
+                    <h3 className="text-center text-[#472192] text-2xl font-bold mt-3">
+                      ATIVIDADES DO ALFABETO
+                    </h3>
+                    <p className="text-center text-[#472192] mt-3 px-5">
+                      Aprender as letras é fundamental para o desenvolvimento de
+                      uma criança. As letras são a base da leitura e da escrita,
+                      e compreendê-las é essencial para o sucesso escolar e para
+                      a comunicação eficiente no futuro.
+                    </p>
+                  </div>
+                </SplideSlide>
+                <SplideSlide>
+                  <div className="w-full bg-[#f7f7f7] min-h-[450px] px-2 py-8 rounded-3xl flex flex-col justify-center items-center">
+                    <img
+                      src="/conteudos/6.jpg"
+                      alt=""
+                      className="w-full max-w-[250px]"
+                    />
+                    <h3 className="text-center text-[#472192] text-2xl font-bold mt-3">
+                      LIVRO DE INDICAÇÃO PELO MEC
+                    </h3>
+                    <p className="text-center text-[#472192] mt-3 px-5">
+                      Métodos de aprendizado infantil recomendados pelo MEC
+                    </p>
+                  </div>
                 </SplideSlide>
               </Splide>
             </div>
@@ -196,9 +297,9 @@ const Home: NextPage = () => {
                 aria-label="My Favorite Images"
                 options={{
                   rewind: true,
-                  padding: "50px",
+                  padding: "30px",
 
-                  gap: "20px",
+                  gap: "10px",
                   perPage: 3,
                   breakpoints: {
                     870: {
@@ -211,13 +312,58 @@ const Home: NextPage = () => {
                 }}
               >
                 <SplideSlide>
-                  <div className="w-full bg-white h-[350px] rounded-3xl "></div>
+                  <div className="w-full bg-white h-auto min-h-[450px] rounded-3xl p-2 py-4 flex flex-col justify-center items-center">
+                    <div className="w-[90%] rounded-3xl h-[200px] bg-[url(/depoimentos/2.png)] bg-no-repeat bg-cover bg-center"></div>
+                    <h3 className="text-center text-[#472192] text-xl font-bold mt-3">
+                      Margarida, Teresina/PI
+                    </h3>
+                    <p className="text-center text-[#472192] mt-3 px-5">
+                      "Não poderia estar mais satisfeito com a minha recente
+                      aquisição da apostila da Universo Kids. O material é
+                      simplesmente excepcional, com uma variedade incrível de
+                      atividades que com certeza vão ajudar no aprendizado dos
+                      meus filhos. Parabenizo a equipe da Universo Kids pelo seu
+                      trabalho dedicado em fornecer produtos de alta qualidade
+                      para a educação das crianças. Se você está em busca de uma
+                      solução completa e eficiente para ensinar seus filhos de
+                      forma lúdica."
+                    </p>
+                  </div>
                 </SplideSlide>
                 <SplideSlide>
-                  <div className="w-full bg-white h-[350px] rounded-3xl "></div>
+                  <div className="w-full bg-white h-auto min-h-[450px] rounded-3xl p-2 py-4 flex flex-col justify-center items-center">
+                    <div className="w-[90%] rounded-3xl h-[200px] bg-[url(/depoimentos/1.png)] bg-no-repeat bg-cover bg-center"></div>
+                    <h3 className="text-center text-[#472192] text-xl font-bold mt-3">
+                      Prof. Tamires Rio do Sul, SC
+                    </h3>
+                    <p className="text-center text-[#472192] mt-3 px-5">
+                      ”Adquirir a Apostila foi a melhor escolha que fiz para
+                      compartilhar momentos divertidos e educativos com meus
+                      filhos. Fiquei impressionado com a qualidade das
+                      atividades e posso afirmar que atendeu todas as minhas
+                      expectativas. Com certeza, é um investimento que vale
+                      muito a pena para quem busca uma forma eficiente e lúdica
+                      de ensinar. Já recomendei para outras famílias e
+                      continuarei indicando essa excelente opção."
+                    </p>
+                  </div>
                 </SplideSlide>
+
                 <SplideSlide>
-                  <div className="w-full bg-white h-[350px] rounded-3xl "></div>
+                  <div className="w-full bg-white h-auto  min-h-[450px] rounded-3xl p-2 py-4 flex flex-col justify-center items-center">
+                    <div className="w-[90%] rounded-3xl h-[200px] bg-[url(/depoimentos/3.png)] bg-no-repeat bg-cover bg-center"></div>
+                    <h3 className="text-center text-[#472192] text-xl font-bold mt-3">
+                      Flávia, Salvador/BA
+                    </h3>
+                    <p className="text-center text-[#472192] mt-3 px-5">
+                      "Comprei a Apostila ABC para meu filho e não poderia estar
+                      mais feliz com a qualidade do material. O preço é
+                      acessível e justo, e o mais importante: meu filho se
+                      divertiu enquanto aprendia. As atividades são tão
+                      envolventes que já as utilizamos diversas vezes sem perder
+                      o interesse."
+                    </p>
+                  </div>
                 </SplideSlide>
               </Splide>
             </div>
@@ -302,6 +448,7 @@ const Home: NextPage = () => {
                 </p>
               </div>
               <Link
+                onClick={() => handleClick()}
                 href={"https://pay.kiwify.com.br/QZaFymQ"}
                 className="mt-10 rounded-full  bg-[#FFC700] px-16 mb-5 py-4 "
               >
